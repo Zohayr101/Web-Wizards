d626f24cd973dac24e2da04c07ff0d37694f607c
Fima Strumban <strumban@oakland.edu>
2025-01-14 21:29:08 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const openPopup = document.getElementById('add_task');
+const closePopup = document.getElementById('close_task');
+const popup = document.getElementById('task_popup');
+
+openPopup.addEventListener('click', () => {
+    popup.style.display = 'flex';
+});
+
+closePopup.addEventListener('click', () => {
+    popup.style.display = 'none';
+});
+
+window.addEventListener('click', (event) => {
+    if (event.target === popup) {
+        popup.style.display = 'none';
+    }
+});
