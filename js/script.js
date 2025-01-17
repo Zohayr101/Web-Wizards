2e4806beba4a664976c644ea6e62758a4f6c60eb
TheyGoPoof <bwood1178@gmail.com>
2025-01-16 20:06:07 -0500

    tall main task

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
+
 const openPopup = document.getElementById('add_task');
 const closePopup = document.getElementById('close_task');
 const popup = document.getElementById('task_popup');
+
 
 openPopup.addEventListener('click', () => {
     popup.style.display = 'flex';
 });
 
+
 closePopup.addEventListener('click', () => {
     popup.style.display = 'none';
 });
+
 
 window.addEventListener('click', (event) => {
     if (event.target === popup) {
