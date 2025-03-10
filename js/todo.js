7fe4e7be63e86addbce21d5461b1088d16b1c632
strumban-edu <strumban@oakland.edu>
2025-03-10 17:56:59 -0400

    Update todo.js

--- js/todo.js
+++ js/todo.js
@@ -229,45 +229,15 @@ function toggleHabit(habitId) {
   localStorage.setItem("habits", JSON.stringify(widgetData["habits"]));
 }
 
-function cycleLayout() {
-  const content = document.getElementsByClassName("main-content")[0];
-  let currentLayout = content.id;
-  let nextLayoutIndex = (layouts.indexOf(currentLayout) + 1) % layouts.length;
-  content.id = layouts[nextLayoutIndex];
-}
-
-function setLayout(layout) {
-  document.getElementsByClassName("main-content")[0].id = layout;
+function setLayout() {
+  layout = localStorage.getItem("layout");
   if (layout === "layout-week-calendar") {
     calendarDisplay = "week";
     initCalendar();
-  } else if (calendarDisplay !== "month") {
+  } else {
     calendarDisplay = "month";
     initCalendar();
   }
-}
-
-function toggleTheme() {
-  const body = document.body;
-
-  for (let t of themes) {
-    if (body.classList.contains(t)) {
-      body.classList.remove(t);
-      body.classList.add(themes[(themes.indexOf(t) + 1) % themes.length]);
-      localStorage.setItem("theme", t);
-      break;
-    }
-  }
-}
-
-function setTheme(theme) {
-  const body = document.body;
-  localStorage.setItem("theme", theme);
-  // remove the current theme
-  for (let t of themes) {
-    body.classList.remove(t);
-  }
-  body.classList.add(theme);
 }
 
 function initCalendar() {
@@ -362,8 +332,7 @@ window.addEventListener("load", () => {
     renderWidget(page, 0);
   });
 
-  // Initialize Calendar at current month, year
-  initCalendar();
+  setLayout();
 
   // Initialize quote cycling on window load
   // Start cycling motivational quotes
